c237fb33ec2575cfa4352837b8debfea3bf57b7d
stoppedwumm <mobileboxnews@icloud.com>
2025-05-10 17:58:27 +0200

    fix: optimiere die Überprüfung des Installationsstatus für Mods im Suchergebnis

--- ui/chrome/js/moddashboard.js
+++ ui/chrome/js/moddashboard.js
@@ -10,10 +10,7 @@ document.getElementById("search").addEventListener("click", async function() {
     for (var i = 0; i < result.length; i++) {
         let project = result[i]
         let projectDiv = document.createElement("div")
-        mods.forEach(mod => {
-            console.log(new String(mod.metadata[0].id).replaceAll("-", ""))
-        })
-        let modInstalled = mods.find(mod => new String(mod.metadata[0].id).replaceAll("-", "") == project.slug) ? true : false
+        let modInstalled = mods.find(mod => new String(mod.metadata[0].id) == new String(project.slug).replaceAll("-", "")) ? true : false
         console.log(modInstalled)
         projectDiv.className = "project"
         projectDiv.innerHTML = `
